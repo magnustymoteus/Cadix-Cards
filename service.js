da3df98aefa3bbc41faf8e89e1c21ea5f7fde7e2
magnustymoteus <gruzixpotato@gmail.com>
2021-11-15 08:40:54 +0100

    2.0.1
    Removed unnecessary console log

--- service.js
+++ service.js
@@ -95,7 +95,6 @@ export function getUser(id) {
 export function updateKlant(data, id) {
     return new Promise(function (resolve, rej) {
         con.query("UPDATE tblklant SET naam=?, voornaam=?, geboortedatum=?, geslacht=?, straat=?,huisnummer=?,bus=?,postcode=?,gemeente=?,land=?,telefoonnummer=?,email=?,admin=? WHERE klantID = ?",  [data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],data[12],id], (err,res) => {
-            console.log(data,id);
             if(err) return rej(err);
             resolve(res);
         });
